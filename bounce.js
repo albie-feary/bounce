25117f5f186ffb9b949964a8604a2909e59cc774
Albie-feary <laurabeth.feary@gmail.com>
2017-04-17 12:10:59 +1200

    add html structure and canvas Id

---
+++ bounce.js
@@ -0,0 +1,10 @@
+var context;
+function init()
+{
+  context= myCanvas.getContext('2d');
+  context.beginPath();
+  context.fillStyle="orange";
+  // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+  context.arc(100,100,20,0,Math.PI*2,true); context.closePath();
+  context.fill();
+}
