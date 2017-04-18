3bbb363247553ca6c65ae3d8f338fd4d67972e2b
Albie-feary <laurabeth.feary@gmail.com>
2017-04-18 18:48:03 +1200

    add three bouncing balls

--- bounce.js
+++ bounce.js
@@ -1,30 +1,51 @@
 var context;
-// global variable coordinates
-var x = 100;
-var y = 200;
-var dx = 5;
-var dy = 5;
+// global variable coordinates for three balls
+// where initial x is start, y is finish(before it bounds), dx is speed and dy is angle of bounce
+var x = 100;var y = 200;var dx = 5;var dy =5;
+var x_1=500;var y_1=300;var dx_1=5;var dy_1=5;
+var x_2=200;var y_2=500;var dx_2=5;var dy_2=5;
 
 // function to iniatiate the drawing of circle.
 function init() {
   // getContext method returns an object context, that provides the methods and properties for drawingon the canvas, ('2d') is a context type parameter
   context= myCanvas.getContext('2d');
-  // call the draw function every 10ms using setInterval method
-  setInterval(draw,10);
+  // call the draw function every 20ms using setInterval method
+  setInterval(draw,25);
 }
 
 function draw() {
   // clearReact method clears the previous circle before it draws a new one.
-  context.clearRect(0,0,800,600);
+  context.clearRect(0,0,500,500);
   context.beginPath();
   context.fillStyle="orange";
-  // Draw a circle of radius 20 at the coordinates 100,200 on the canvas using the arc and fill methods
+  context.beginPath();
   context.arc(x,y,20,0,Math.PI*2,false);
-  context.closePath();
   context.fill();
-  // Boundary logic, check if x and y are beyond the canvas dimensions. If so, then we reverse the direction by setting values of dx and dy to the negative values.
-  if(x<0 || x>800) dx=-dx;
-  if(y<0 || y>600) dy=-dy;
+  context.beginPath();
+  context.arc(x_1,y_1,10,0,Math.PI*2,false);
+  context.fill();
+  context.beginPath();
+  context.arc(x_2,y_2,30,0,Math.PI*2,false);
+  context.fill();
+  context.closePath();
+
+  boundaryLogic();
+}
+
+// Boundary logic, check if x and y are beyond the canvas dimensions. If so, then we reverse the direction by setting values of dx and dy to the negative values.
+function boundaryLogic(){
+  if(x<0 || x>500) dx=-dx;
+  if(y<0 || y>500) dy=-dy;
   x+=dx;
   y+=dy;
+
+  if (x_1<0 || x_1>500) dx_1 = -dx_1;
+  if (y_1<0 || y_1>500) dy_1 = -dy_1;
+  x_1 += dx_1;
+  y_1 += dy_1;
+
+  if (x_2<0 || x_2>500) dx_2 = -dx_2;
+  if (y_2<0 || y_2>500) dy_2 = -dy_2;
+  x_2 += dx_2;
+  y_2 += dy_2;
 }
