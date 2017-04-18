4497142fe159261b500f8fd78a6bc87c7ccb1281
Albie-feary <laurabeth.feary@gmail.com>
2017-04-18 18:10:13 +1200

    add single bouncing ball

--- bounce.js
+++ bounce.js
@@ -1,4 +1,3 @@
-// global variable
 var context;
 // global variable coordinates
 var x = 100;
@@ -8,23 +7,24 @@ var dy = 5;
 
 // function to iniatiate the drawing of circle.
 function init() {
+  // getContext method returns an object context, that provides the methods and properties for drawingon the canvas, ('2d') is a context type parameter
   context= myCanvas.getContext('2d');
-  // call the draw function every 10ms
+  // call the draw function every 10ms using setInterval method
   setInterval(draw,10);
 }
 
 function draw() {
   // clearReact method clears the previous circle before it draws a new one.
-  context.clearRect(0,0,300,300);
+  context.clearRect(0,0,800,600);
   context.beginPath();
   context.fillStyle="orange";
-  // Draw a circle of radius 20 at the coordinates 100,100 on the canvas using the arc and fill methods
+  // Draw a circle of radius 20 at the coordinates 100,200 on the canvas using the arc and fill methods
   context.arc(x,y,20,0,Math.PI*2,false);
   context.closePath();
   context.fill();
   // Boundary logic, check if x and y are beyond the canvas dimensions. If so, then we reverse the direction by setting values of dx and dy to the negative values.
-  if(x<0 || x>300) dx=-dx;
-  if(y<0 || y>300) dy=-dy;
+  if(x<0 || x>800) dx=-dx;
+  if(y<0 || y>600) dy=-dy;
   x+=dx;
   y+=dy;
 }
